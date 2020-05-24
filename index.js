2a13dd9a8b1c5666b73134de535b4db45ab189ab
Cameron Dubas <camerondubas@gmail.com>
2020-05-23 20:38:43 -0700

    Episode 1

---
+++ index.js
@@ -0,0 +1,31 @@
+const createMemory = require("./create-memory");
+const CPU = require("./cpu");
+const instructions = require("./instructions");
+
+const memory = createMemory(256);
+const writeableBytes = new Uint8Array(memory.buffer);
+
+const cpu = new CPU(memory);
+
+writeableBytes[0] = instructions.MOV_LIT_R1;
+writeableBytes[1] = 0x12; // 0x1234
+writeableBytes[2] = 0x34;
+
+writeableBytes[3] = instructions.MOV_LIT_R2;
+writeableBytes[4] = 0xab; // 0xABCD
+writeableBytes[5] = 0xcd;
+
+writeableBytes[6] = instructions.ADD_REG_REG;
+writeableBytes[7] = 2; // r1 index
+writeableBytes[8] = 3; // r2 index
+
+cpu.debug();
+
+cpu.step();
+cpu.debug();
+
+cpu.step();
+cpu.debug();
+
+cpu.step();
+cpu.debug();
