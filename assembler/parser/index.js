e350692770686b6bd40732868693c8403dac5a9a
Cameron Dubas <camerondubas@gmail.com>
2020-05-31 22:29:08 -0700

    Episode 9

--- assembler/parser/index.js
+++ assembler/parser/index.js
@@ -19,6 +19,62 @@ const peek = A.lookAhead(A.regex(/^./));
 const upperOrLowerString = (str) =>
   A.choice([A.str(str.toUpperCase()), A.str(str.toLowerCase())]);
 
+const binaryOperation = asType("BINARY_OPERATION");
+const disambiguateOrderOfOperations = (expression) => {
+  if (
+    expression.type !== "SQUARE_BRACKET_EXPRESSION" &&
+    expression.type !== "BRACKETED_EXPRESSION"
+  ) {
+    return expression;
+  }
+
+  if (expression.value.length === 1) {
+    return expression.value[0];
+  }
+
+  const priorities = {
+    OP_MULTIPLY: 2,
+    OP_PLUS: 1,
+    OP_MINUS: 0,
+  };
+
+  let candidateExpression = {
+    priority: -Infinity,
+  };
+
+  // Use index = 1 and index += 2 because this way "index" is always pointing at an operator
+  // Ex: [$42, +, !loc, *, $31, -, $C0]
+  for (let index = 1; index < expression.value.length; index += 2) {
+    const operation = expression.value[index];
+    const level = priorities[operation.type];
+
+    if (level > candidateExpression.priority) {
+      candidateExpression = {
+        priority: level,
+        leftHandSideOperand: index - 1, // Left-hand-side operand
+        rightHandSideOperand: index + 1, // Right-hand-side operand
+        operation,
+      };
+    }
+  }
+
+  const newExpression = asType("BRACKETED_EXPRESSION")([
+    ...expression.value.slice(0, candidateExpression.leftHandSideOperand),
+    binaryOperation({
+      leftHandSideOperand: disambiguateOrderOfOperations(
+        expression.value[candidateExpression.leftHandSideOperand]
+      ),
+      rightHandSideOperand: disambiguateOrderOfOperations(
+        expression.value[candidateExpression.rightHandSideOperand]
+      ),
+      operation: candidateExpression.operation,
+    }),
+    ...expression.value.slice(candidateExpression.rightHandSideOperand + 1),
+  ]);
+
+  return disambiguateOrderOfOperations(newExpression);
+};
+
 const register = A.choice([
   upperOrLowerString("r1"),
   upperOrLowerString("r2"),
@@ -49,6 +105,9 @@ const operator = A.choice([
   A.char("*").map(asType("OP_MULTIPLY")),
 ]);
 
+// Returns the last element in an array
+const last = (arr) => arr[arr.length - 1];
+
 // A valid identifier is a letter or underscore, optionally followed by a letter, number, or underscore
 const validIdentifier = mapJoin(
   A.sequenceOf([
@@ -63,8 +122,81 @@ const variable = A.char("!")
   .chain(() => validIdentifier)
   .map(asType("VARIABLE"));
 
-// TODO, next time
-const bracketedExpression = () => {};
+const typifyBracketedExpression = (expression) => {
+  const asBracketed = asType("BRACKETED_EXPRESSION");
+
+  return asBracketed(
+    expression.map((element) => {
+      if (Array.isArray(element)) {
+        return typifyBracketedExpression(element);
+      }
+      return element;
+    })
+  );
+};
+
+const bracketedExpression = A.coroutine(function* () {
+  const states = {
+    OPEN_BRACKET: 0,
+    OPERATOR_OR_CLOSING_BRACKET: 1,
+    ELEMENT_OR_OPENING_BRACKET: 2,
+    CLOSE_BRACKET: 3,
+  };
+
+  let state = states.ELEMENT_OR_OPENING_BRACKET;
+
+  const expression = [];
+  const stack = [expression];
+  yield A.char("(");
+
+  while (true) {
+    const nextChar = yield peek;
+    if (state === states.OPEN_BRACKET) {
+      yield A.char("(");
+      expression.push([]);
+      stack.push(last(expression));
+      yield A.optionalWhitespace;
+
+      state = states.ELEMENT_OR_OPENING_BRACKET;
+    } else if (state === states.CLOSE_BRACKET) {
+      yield A.char(")");
+      stack.pop();
+      if (stack.length === 0) {
+        // We've reached the end of the bracked expression
+        break;
+      }
+
+      yield A.optionalWhitespace;
+      state = states.OPERATOR_OR_CLOSING_BRACKET;
+    } else if (state === states.ELEMENT_OR_OPENING_BRACKET) {
+      if (nextChar === ")") {
+        yield A.fail("Unexpected end of expression");
+      }
+
+      if (nextChar === "(") {
+        state = states.OPEN_BRACKET;
+      } else {
+        last(stack).push(yield A.choice([hexLiteral, variable]));
+        yield A.optionalWhitespace;
+        state = states.OPERATOR_OR_CLOSING_BRACKET;
+      }
+    } else if (state === states.OPERATOR_OR_CLOSING_BRACKET) {
+      if (nextChar === ")") {
+        state = states.CLOSE_BRACKET;
+        continue;
+      }
+
+      last(stack).push(yield operator);
+      yield A.optionalWhitespace;
+      state = states.ELEMENT_OR_OPENING_BRACKET;
+    } else {
+      // This shouldn't happen!
+      throw new Error("Unknown state");
+    }
+  }
+
+  return typifyBracketedExpression(expression);
+});
 
 const squareBracketExpression = A.coroutine(function* () {
   yield A.char("[");
@@ -86,7 +218,7 @@ const squareBracketExpression = A.coroutine(function* () {
         variable,
       ]);
       expression.push(result);
-      state = EXPECT_OPERATOR;
+      state = states.EXPECT_OPERATOR;
       yield A.optionalWhitespace;
     } else if (state === states.EXPECT_OPERATOR) {
       const nextChar = yield peek;
@@ -104,7 +236,7 @@ const squareBracketExpression = A.coroutine(function* () {
   }
 
   return asType("SQUARE_BRACKET_EXPRESSION")(expression);
-});
+}).map(disambiguateOrderOfOperations);
 
 // mov [$42 + !loc - ($05 * $31)], r1
 const movLitToReg = A.coroutine(function* () {
@@ -126,5 +258,8 @@ const movLitToReg = A.coroutine(function* () {
   });
 });
 
-const res = movLitToReg.run("mov $42, r4");
+// const res = movLitToReg.run("mov $42, r4");
+const res = movLitToReg.run(
+  "mov [$42 + !loc - ($05 * ($31 + !var) - $07)], r4"
+);
 deepLog(res);
